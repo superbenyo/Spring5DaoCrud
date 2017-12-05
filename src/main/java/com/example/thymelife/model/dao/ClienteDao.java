917cafeef500d6d0bb2a368320a109871fd54951
Super Benyo <mavx@praxis.com.mx>
2017-12-04 18:08:19 -0600

    Proyecto spring con el dao cambiado a un crud

---
+++ src/main/java/com/example/thymelife/model/dao/ClienteDao.java
@@ -0,0 +1,11 @@
+package com.example.thymelife.model.dao;
+
+import com.example.thymelife.model.entity.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by administrador on 4/12/17.
+ */
+public interface ClienteDao extends CrudRepository<Cliente, Long>{
+
+}
